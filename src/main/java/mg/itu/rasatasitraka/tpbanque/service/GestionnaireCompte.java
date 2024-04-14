d99d7f3d8975673e81d3cd365fcde665d7236db4
SitrakaRasata45 <rasatasitraka2@gmail.com>
2024-04-14 15:41:30 +0300

    Création et paramétrage du bean CDI GestionnaireCompte

---
+++ src/main/java/mg/itu/rasatasitraka/tpbanque/service/GestionnaireCompte.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.rasatasitraka.tpbanque.service;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import mg.itu.rasatasitraka.tpbanque.entity.CompteBancaire;
+
+/**
+ *
+ * @author Sitraka Rasata
+ */
+@DataSourceDefinition(
+        className = "com.mysql.cj.jdbc.MysqlDataSource",
+        name = "java:app/jdbc/banque",
+        serverName = "localhost",
+        portNumber = 3306,
+        user = "rasata_sitraka_45", // nom et
+        password = "root", // mot de passe que vous avez donnés lors de la création de la base de données
+        databaseName = "banque",
+        properties = {
+            "useSSL=false",
+            "allowPublicKeyRetrieval=true",
+            "driverClass=com.mysql.cj.jdbc.Driver"
+        }
+)
+@RequestScoped
+public class GestionnaireCompte {
+
+    @PersistenceContext(unitName = "banquePU")
+    private EntityManager em;
+
+    @Transactional
+    public void creerCompte(CompteBancaire compteBancaire) {
+        em.persist(compteBancaire);
+    }
+
+    public List<CompteBancaire> getAllComptes() {
+        TypedQuery<CompteBancaire> query = em.createNamedQuery("CompteBancaire.findAll", CompteBancaire.class);
+        return query.getResultList();
+    }
+}
