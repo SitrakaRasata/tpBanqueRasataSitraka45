aa548bc8c56610cac838217d10463e4871b7f98b
SitrakaRasata45 <rasatasitraka2@gmail.com>
2024-04-15 21:53:35 +0300

    Ajout de mouvements sur un compte bancaire

--- src/main/java/mg/itu/rasatasitraka/tpbanque/service/GestionnaireCompte.java
+++ src/main/java/mg/itu/rasatasitraka/tpbanque/service/GestionnaireCompte.java
@@ -74,4 +74,28 @@ public class GestionnaireCompte {
     public void supprimerCompte(CompteBancaire compte) {
         em.remove(em.merge(compte));
     }
+
+    /**
+     * Dépôt d'argent sur un compte bancaire.
+     *
+     * @param compteBancaire
+     * @param montant
+     */
+    @Transactional
+    public void deposer(CompteBancaire compteBancaire, int montant) {
+        compteBancaire.deposer(montant);
+        update(compteBancaire);
+    }
+
+    /**
+     * Retrait d'argent sur un compte bancaire.
+     *
+     * @param compteBancaire
+     * @param montant
+     */
+    @Transactional
+    public void retirer(CompteBancaire compteBancaire, int montant) {
+        compteBancaire.retirer(montant);
+        update(compteBancaire);
+    }
 }
